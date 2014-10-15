cddc49e035dc534a3136aeeee43f26c110f08a1f
Crobays <crobays@userex.nl>
2014-10-15 12:16:19 +0000

    enhance timezone script

--- config-scripts/timezone.sh
+++ config-scripts/timezone.sh
@@ -29,44 +29,56 @@ function error ()
     printf "$NC"
 }
 
-if [ $LOG ];then
+if [ $LOG_LEVEL -gt 0 ]
+then
 	echo "-- Timezone start --"
 fi
 
 timezone_file="/etc/timezone"
 
 timezone="$1"
-if [ $timezone ];then
-	if [ -f $timezone_file.bak ];then
-		cp -f $timezone_file.bak $timezone_file
-	elif [ -f $timezone_file ];then
-		cp -f $timezone_file $timezone_file.bak
+if [ "$timezone" != "" ]
+then
+	if [ -f "$timezone_file.bak" ]
+	then
+		cp -f "$timezone_file.bak" "$timezone_file"
+	elif [ -f "$timezone_file" ]
+	then
+		cp -f "$timezone_file" "$timezone_file.bak"
 	else
-		echo "$timezone" > $timezone_file
+		echo "$timezone" > "$timezone_file"
 	fi
 	
-	if [ "$(cat $timezone_file)" != "$timezone" ];then
-		echo "$timezone" > $timezone_file
+	if [ "$(cat $timezone_file)" != "$timezone" ]
+	then
+		echo "$timezone" > "$timezone_file"
 		dpkg-reconfigure --frontend noninteractive tzdata &>-
 		rm ./-
 	fi
-	if [ $LOG ];then
-		info "Time zone:       $timezone"
+
+	if [ $LOG_LEVEL -gt 0 ]
+	then
+		info    "Time zone:       $timezone"
 		echo -e "Local time:      $(date)"
 		echo -e "Universal time:  $(date -u)"
 	fi
-	if [ -f /etc/php5/fpm/php.ini ];then
+
+	if [ -f /etc/php5/fpm/php.ini ]
+	then
 		sed -i "s/;date.timezone =/date.timezone =/" /etc/php5/fpm/php.ini
 		sed -i "s/date.timezone =.*/date.timezone = ${timezone//\//\\\/}/" /etc/php5/fpm/php.ini
 	fi
 
-	if [ -f /etc/php5/cli/php.ini ];then
+	if [ -f /etc/php5/cli/php.ini ]
+	then
 		sed -i "s/;date.timezone =/date.timezone =/" /etc/php5/cli/php.ini
 		sed -i "s/date.timezone =.*/date.timezone = ${timezone//\//\\\/}/" /etc/php5/cli/php.ini
 	fi
 else
 	error "Missing argument: timezone"
 fi
-if [ $LOG ];then
+
+if [ $LOG_LEVEL -gt 0 ]
+then
 	echo "-- Timezone end --"
 fi
