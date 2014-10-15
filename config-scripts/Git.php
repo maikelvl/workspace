0cb1d3b06d2d4e0e6c41f7d886479f688d3afa26
Crobays <crobays@userex.nl>
2014-10-15 15:50:40 +0000

    better git resetting after first run

--- config-scripts/Git.php
+++ config-scripts/Git.php
@@ -144,8 +144,9 @@ class Git {
 			if (is_dir($tmp_config_git_dir))
 			{
 				rename($tmp_config_git_dir.'/.git', CONFIG_DIR.'/.git');
-				exec('git --git-dir '.CONFIG_DIR.'/.git reset --hard');
 				rrmdir($tmp_config_git_dir);
+				sleep(2);
+				exec('git --git-dir '.CONFIG_DIR.'/.git reset --hard');
 			}
 			Logger::log("Cloned config repo $repo_name from $host", 1);
 		}
