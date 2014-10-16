861f7e3aae0f3090a8efcd36882b5ced6f6d2c55
Maikel Vlasman <gitlab.com-pkaqihs1fe1ax8on@maikelvlasman.com>
2014-10-16 16:04:58 +0000

    fix download-and-install script path

--- base/provider/packer.sh
+++ base/provider/packer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /workspace/base/utilities/download-and-install.sh
+source /scripts/utilities/download-and-install.sh
 download_and_install \
 	packer \
 	http://www.packer.io/downloads.html \
