e46ff1adda68af997a1cfb3ecaf05ac4d693efc4
Crobays <crobays@userex.nl>
2014-10-16 13:07:58 +0000

    fix bug where download-and-install did not execute at installing node, packer and terraform

--- base/utilities/download-and-install.sh
+++ base/utilities/download-and-install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 DOWNLOADS_DIRECTORY="/downloads"
-SILENT="no"
+SILENT_LEVEL=2
+
+# === Download and install scripts: ================================
 
 black="\e[0;30m"
 red="\e[0;31m"
 green="\e[0;32m"
 orange="\e[0;33m"
-blue="\e[0;34m"
+blue="\e[1;34m"
 purple="\e[0;35m"
 turquase="\e[0;36m"
 NC="\e[0m"
@@ -15,6 +17,18 @@ function info ()
 {
     printf "$turquase"
     echo "$1"
+    printf "$NC"
+}
+
+function ask ()
+{
+    printf "$blue"
+    if [ "$1" == "-n" ]
+    then
+    	echo -n "$2"
+	else
+		echo "$1"
+	fi
     printf "$NC"
 }
 
@@ -39,13 +53,11 @@ function error ()
     printf "$NC"
 }
 
-# === Download and install scripts: ================================
-
-download_and_install ()
+function download_and_install ()
 {
 	command="$1"
 	downloads_link="$2"
-	download_link_pattern="$3"
+	download_link_base_pattern="$3"
 	specific_version="$4"
 	application_name="$5"
 
@@ -72,7 +84,8 @@ download_and_install ()
 		echo "Missing second argument: downloads link"
 		exiting="yes"
 	fi
-	if [ "$download_link_pattern" == "" ]
+
+	if [ "$download_link_base_pattern" == "" ]
 	then
 		echo "Missing third argument: download link pattern"
 		exiting="yes"
@@ -83,7 +96,8 @@ download_and_install ()
 		exit
 	fi
 
-	if [ "$application_name" == "" ] && [ "$command" != "" ];then
+	if [ "$application_name" == "" ] && [ "$command" != "" ]
+	then
 		application_name=$(echo $command | head -c 1 | tr [a-z] [A-Z]; echo $1 | tail -c +2)
 	fi
 
@@ -102,8 +116,8 @@ download_and_install ()
 		fi
 	fi
 
-	echo "Fetching downloads page: $downloads_link..."
-	download_link="$(find_download_link "$downloads_link" "$download_link_pattern")"
+	info "Fetching downloads page $downloads_link..."
+	download_link="$(find_download_link "$downloads_link" "$download_link_base_pattern")"
 	if [ "$download_link" == "" ]
 	then
 		error "No download link found on $downloads_link"
@@ -137,33 +151,40 @@ download_and_install ()
 				download_link="$domain_name$download_link"
 			fi
 			dest="$DOWNLOADS_DIRECTORY/$(basename $download_link)"
-			mkdir -p $(dirname $DOWNLOADS_DIRECTORY)
+			sudo mkdir -p $(dirname $DOWNLOADS_DIRECTORY)
 			if [ ! -f "$dest" ]
 			then
-				download "$application_name" "$download_link" "$dest"
+				download \
+					"$download_link" \
+					"$dest" \
+					"$application_name"
 			fi
-			install "$dest" "$application_name"
+			install \
+				"$command" \
+				"$dest" \
+				"$application_name"
 		elif [ "$current_version" != "" ]
 		then
-			success "$application_name already on latest version: $current_version"
+			info "$application_name already on latest version: $current_version"
 		else
 			success "$application_name already installed"
 		fi
-		info "Download link: $download_link"
+		info "Downloaded $download_link"
 	
 	fi
 
 	echo "-- $application_name end --"
 }
 
-download()
+function download()
 {
-	application_name="$1"
-	download_link="$2"
-	dest="$3"
+	download_link="$1"
+	dest="$2"
+	application_name="$3"
 
 	minus_one="-1"	
-	if [ "${dest:$minus_one:1}" == "/" ];then
+	if [ "${dest:$minus_one:1}" == "/" ]
+	then
 		dest="$dest$(basename $download_link)"
 	fi
 
@@ -173,17 +194,19 @@ download()
 	fi
 
 	info "Downloading latest $application_name $download_link..."
-	if [ "$SILENT" == "yes" ]
+	silent_flag=""
+	if [ $SILENT_LEVEL -gt 0 ]
 	then
 		silent_flag="--silent"
 	fi
 	curl --location $silent_flag --output "$dest" --url "$download_link"
 }
 
-install()
+function install()
 {
-	package="$1"
-	application_name="$2"
+	command="$1"
+	package="$2"
+	application_name="$3"
 
 	filename=$(basename "$package")
 	extension="${filename##*.}"
@@ -192,7 +215,8 @@ install()
 	volume_name="${volume_name%.*}"
 	volume_name="${volume_name// /-}"
 	volume_name="$(echo $volume_name | awk '{print tolower($0)}')"
-	extraction_path="$HOME/$volume_name-extraction"
+	extraction_path="$HOME/${filename%.*}-extraction"
+	mkdir -p "$extraction_path"
 	usr="/usr/local"
 	sudo mkdir -p "$usr"
 
@@ -200,24 +224,17 @@ install()
 		dmg)
 			volume_path="/Volumes/$volume_name"
 			hdiutil attach -mountpoint "$volume_path" "$package"
-			pkg="$(ls $volume_path/*.pkg)"
-			if [ "$pkg" != "" ]
-			then
-				sudo installer -verboseR -pkg "$pkg" -target /
-			fi
+			osascript -e 'tell application "Finder"' -e 'close front window' -e 'end tell'
+			echo "Please wait a sec..."
+			sudo cp -rf $volume_path/* $extraction_path/
 			hdiutil detach "$volume_path"
-			if [ "$pkg" == "" ]
-			then
-				error "No package found in $volume_path: $pkg"
-				exit
-			fi
 			;;
 		zip)
 			if [ "$(which apt-get)" != "" ]
 	    	then
 	    		apt-get install unzip
 			fi
-			mkdir "$extraction_path"
+			
 			unzip -o "$package" -d "$extraction_path"
 			;;
 		gz|bz2)
@@ -225,7 +242,6 @@ install()
 			then
 				rm -rf "$extraction_path"
 			fi
-			mkdir "$extraction_path"
 			case "$extension" in
 				gz)
 					method="z"
@@ -238,8 +254,7 @@ install()
 			remove_sub_dir "$extraction_path"
 			;;
 		vbox-extpack)
-			cp -f "$package" "$WORKSPACE/.extension.vbox-extpack" 
-			return
+			cp -f "$package" "$WORKSPACE/.system/$(basename "$package")" 
 			;;
 		?)
 			error "Unable to install $extension-files"
@@ -247,71 +262,86 @@ install()
 			;;
 	esac
 
+	application_path="Applications"
+	dest_application_path="/$application_path"
+	app_path=""
+	pkg="$(ls $extraction_path | grep .pkg | head -1)"
+	app="$(ls $extraction_path | grep .app | head -1)"
+	if [ "$pkg" != "" ]
+	then
+		sudo installer -verboseR -pkg "$extraction_path/$pkg" -target /
+		uninstall_script="$(ls $extraction_path | grep .tool | head -1)"
+		if [ "$uninstall_script" != "" ]
+		then
+			sudo cp -f "$extraction_path/$uninstall_script" "$WORKSPACE/.system/uninstall-$volume_name.sh"
+			sudo chmod +x "$WORKSPACE/.system/uninstall-$volume_name.sh"
+		fi
+		app_path="$dest_application_path/$application_name"
+	elif [ "$app" != "" ]
+	then
+		app_path="$extraction_path/$app"
+	fi
+
+	# -- Move to home Applications directory if there is one ------------------------
+	if [ "$app_path" != "" ] && [ -d "$app_path" ]
+	then
+		dest_app_path="$dest_application_path/$(basename "$app_path")"
+		if [ "$app_path" != "$dest_app_path" ]
+		then
+			sudo mv -f "$app_path" "$dest_app_path"
+		fi
+
+		success "$application_name installed."
+		add_to_uninstaller "trash \"$dest_app_path\""
+	fi
+
 	if [ "$command" != "" ]
 	then
 		if [ -d "$extraction_path/bin" ]
 		then
-			mv -f "$extraction_path" "$usr/$command"
+			sudo mv -f "$extraction_path" "$usr/$command"
 			export PATH="$PATH:$usr/$command/bin"
 		else
-			mkdir -p "$usr/bin"
-			mv $extraction_path/* "$usr/bin/"
+			sudo mkdir -p "$usr/bin"
+			sudo mv $extraction_path/* "$usr/bin/"
 		fi
 
 		if [ "$(which $command)" != "" ]
 		then
-			success "$application_name $(version $command) installed"
+			success "$application_name $(version $command)"
 		else
 			error "Something went wrong installing $application_name"
 		fi
 	fi
-
-	# -- Move to home Applications directory if there is one ------------------------
-	if [ "$application_name" != "" ]
-	then
-		if [ -d "$HOME/Applications" ] && [ -d "/Applications/$application_name" ]
-		then
-			command_location=""
-			if [ "$command" != "" ] && [ "$(which $command)" != "" ] && [ "$HOME/Applications/$application_name/bin/$command" ]
-			then
-				command_location="$(which $command)"
-			fi
-			sudo mv "/Applications/$application_name" "$HOME/Applications"
-			success "$application_name moved to $HOME/Applications."
-			if [ "$command_location" != "" ]
-			then
-				sudo ln -sf "$HOME/Applications/$application_name/bin/$command" "$command_location"
-			fi
-		elif [ -d "$HOME/Applications/$application_name" ] || [ -d "/Applications/$application_name" ]
-		then
-			success "$application_name already installed."
-		fi
-	fi
-
+	
 	if [ -d "$extraction_path" ]
 	then
-		rm -rf "$extraction_path"
+		sudo rm -rf "$extraction_path"
 	fi
 }
 
-find_download_link()
+function find_download_link()
 {
 	downloads_link="$1"
-	download_link_pattern="$2"
+	download_link_base_pattern="$2"
+	download_link_base_front=""
+	download_link_base_rear=""
+	latest_version=""
 
-	IFS='*' read -ra str <<< "$download_link_pattern"
+	IFS='*' read -ra str <<< "$download_link_base_pattern"
 	for i in "${str[@]}"
 	do
 	    if [ "$download_link_base_front" == "" ]
 	    then
 	    	download_link_base_front="$i"
-	    elif [ "$download_link_rear" == "" ]
+	    elif [ "$download_link_base_rear" == "" ]
 	    then
-	    	download_link_rear="$i"
+	    	download_link_base_rear="$i"
 	    fi
 	done
-
-	if [ 1 ] #"$SILENT" == "yes" ]
+	silent_flag=""
+	silent_flag=""
+	if [ $SILENT_LEVEL -gt 1 ]
 	then
 		silent_flag="--silent"
 	fi
@@ -319,18 +349,18 @@ find_download_link()
 	# Find download link
 	find_download_link_html=$(curl --location $silent_flag --url "$downloads_link")
 	find_download_link_html="${find_download_link_html//href=\"/ }"
-	find_download_link_html="${find_download_link_html//$download_link_rear/$download_link_rear }"
+	find_download_link_html="${find_download_link_html//$download_link_base_rear/$download_link_base_rear }"
 	for s in $find_download_link_html
 	do
-		if [ "${s%$download_link_rear}" != "$s" ]
+		if [ "${s%$download_link_base_rear}" != "$s" ]
 		then
 			file_name="$(basename $s)"
 			latest_version="${file_name/${download_link_base_front}_/}"
 			latest_version="${latest_version/${download_link_base_front}-/}"
 			latest_version="${latest_version/${download_link_base_front}/}"
-			latest_version="${latest_version/_${download_link_rear}/}"
-			latest_version="${latest_version/-${download_link_rear}/}"
-			latest_version="${latest_version/${download_link_rear}/}"
+			latest_version="${latest_version/_${download_link_base_rear}/}"
+			latest_version="${latest_version/-${download_link_base_rear}/}"
+			latest_version="${latest_version/${download_link_base_rear}/}"
 			if [ "$latest_version" == "master" ]
 			then
 				continue
@@ -342,7 +372,7 @@ find_download_link()
 }
 
 # Removes a directory which is in between two directories: /home/subdir/directory becomes /home/directory
-remove_sub_dir ()
+function remove_sub_dir ()
 {
 	if [ ! -d "$1" ]
 	then
