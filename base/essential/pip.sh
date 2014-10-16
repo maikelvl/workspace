d701c18369c239efdb2ad85dcec96d4adba87a3b
Crobays <crobays@userex.nl>
2014-10-16 12:42:55 +0000

    add maestro to workspace base

---
+++ base/essential/pip.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "-- PIP start --"
+
+curl -L https://bootstrap.pypa.io/get-pip.py | python
+sudo apt-get install -y python-yaml python-dev libxml2-dev libxslt-dev
+
+echo "-- PIP end --"
+
