9265c8fb6ba09926828b10e886749b22b999ff43
Maikel Vlasman <gitlab.com-pkaqihs1fe1ax8on@maikelvlasman.com>
2014-11-12 13:00:45 +0100

    update workspace docker

--- base/docker/fig.sh
+++ base/docker/fig.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 echo "-- Fig start --"
 
-if [ "$1" == "install/fig.sh" ];then
-	shift
-fi
+pip install -U fig
 
-version="${1:-0.5.2}"
-dest="/downloads/fig-$version"
-if [ ! -f "$dest" ];then
-	curl --location --silent --url https://github.com/orchardup/fig/releases/download/$version/linux --output "$dest"
-fi
-cp "$dest" /usr/local/bin/fig
-chmod +x /usr/local/bin/fig && echo "$(fig --version) installed"
+# if [ "$1" == "install/fig.sh" ];then
+# 	shift
+# fi
+
+# version="${1:-0.5.2}"
+# dest="/downloads/fig-$version"
+# if [ ! -f "$dest" ];then
+# 	curl --location --silent --url https://github.com/orchardup/fig/releases/download/$version/linux --output "$dest"
+# fi
+# cp "$dest" /usr/local/bin/fig
+# chmod +x /usr/local/bin/fig && echo "$(fig --version) installed"
 
 echo "-- Fig end --"
--- base/run.sh
+++ base/run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+for file in $(ls /etc/my_init.d)
+do
+	/etc/my_init.d/$file
+done
+
 [ -d "/usr/local/node/bin" ] && export PATH="/usr/local/node/bin:$PATH"
 
 bash
