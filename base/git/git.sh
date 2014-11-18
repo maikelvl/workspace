621781da32858a240841208c3bea38e5dae4de4f
Maikel Vlasman <gitlab.com-pkaqihs1fe1ax8on@maikelvlasman.com>
2014-11-18 16:30:34 +0100

    add feature so git grabs the latest version automatically

--- base/git/git.sh
+++ base/git/git.sh
@@ -1,42 +1,78 @@
 #!/bin/bash
 echo "-- Git start --"
 
-version=${1:-2.1.1}
+SILENT_LEVEL=1
 
-reinstall=""
-if [ "$(which git)" == "" ];then
-	reinstall="yes"
-else
-	current_version="$(git --version)"
-	if [ "${current_version:12:5}" != "$version" ];then
-		reinstall="yes"
-	fi
-fi
-
-if [ "$reinstall" == "yes" ];then
-	mkdir -p tmp-git-src
-	cd tmp-git-src
-	if [ "$(which yum)" != "" ];then
-		yum install -y curl-devel expat-devel gettext-devel openssl-devel zlib-devel
-		yum install -y perl-ExtUtils-MakeMaker gcc-c++ make
-	elif [ "$(which apt-get)" != "" ];then
-		apt-get install -y libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev
+function find_download_link()
+{
+	downloads_link="$1"
+	download_link_base_pattern="$2"
+	
+	silent_flag=""
+	if [ $SILENT_LEVEL -gt 1 ]
+	then
+		silent_flag="--silent"
 	fi
 	
-	dest="/downloads/git-$version.tar.gz"
-	mkdir -p $(dirname $dest)
-	if [ ! -f "$dest" ];then
-		curl --silent --location --url https://www.kernel.org/pub/software/scm/git/git-$version.tar.gz --output "$dest"
+	#curl --location $silent_flag --url $downloads_link
+	links="$(curl --location $silent_flag --url $downloads_link | awk -v RS='<a' "/ href=\".*?$download_link_base_pattern\"/{ print \$1 }" | sort)"
+	
+	for link in $links;do
+		last_item="$link"
+	done
+
+	link=$(echo $last_item | awk -F='href="' -v RS=">" '/href=".*"/{ print $1 }' | sed -e 's/href="//' | sed -e 's/"//')
+	
+	if [ "$link" == "" ];then
+		return
 	fi
-	tar -zxf "$dest"
-	cd git-$version
-	make prefix=/usr/local all
-	make prefix=/usr/local install
-	cd ../..
-	rm -rf tmp-git-src
-	echo "git $version fresh installed"
-else
-	echo "$(git --version) was already installed"
+
+	if [ "${link/\/\//}" == "$link" ];then
+		link="$downloads_link$link"
+	fi
+	
+	echo $link
+}
+
+version=$1
+if [ $version ];then
+	version="$version."
 fi
+
+url="$(find_download_link \
+	"https://www.kernel.org/pub/software/scm/git/" \
+	"git-${version//./\\.}([0-9]\.)*tar\.gz")"
+
+if [ "$url" == "" ];then
+	echo "no matching git version found"
+	exit
+fi
+
+file_name="$(basename $url)"
+version="${file_name/git-/}"
+version="${version/.tar.gz/}"
+
+mkdir -p tmp-git-src
+cd tmp-git-src
+if [ "$(which yum)" != "" ];then
+	yum install -y curl-devel expat-devel gettext-devel openssl-devel zlib-devel
+	yum install -y perl-ExtUtils-MakeMaker gcc-c++ make
+elif [ "$(which apt-get)" != "" ];then
+	apt-get install -y libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev
+fi
+
+dest="/downloads/$file_name"
+mkdir -p $(dirname $dest)
+if [ ! -f "$dest" ];then
+	curl --silent --location --url "$url" --output "$dest"
+fi
+tar -zxf "$dest"
+cd git-$version
+make prefix=/usr/local all
+make prefix=/usr/local install
+cd ../..
+rm -rf tmp-git-src
+
+echo "git $version fresh installed"
 
 echo "-- Git end --"
