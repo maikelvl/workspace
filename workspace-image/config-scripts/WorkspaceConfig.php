f836780d12bd7be9517673833707ba4c5380c131
Maikel Vlasman <gitlab.com-pkaqihs1fe1ax8on@maikelvlasman.com>
2014-11-20 15:09:33 +0100

    refactor

--- workspace-image/config-scripts/WorkspaceConfig.php
+++ workspace-image/config-scripts/WorkspaceConfig.php
@@ -2,12 +2,12 @@
 
 class WorkspaceConfig {
 	
-	public function setWorkspaceRepo($services)
+	public function setConfigRepo($services)
 	{
 		foreach($services as $service)
 		{
 			$config = $service->config();
-			if(is_dir(CONFIG_DIR.'/.git')
+			if(is_dir(CONFIG_DIR.'/.git'))
 			{
 				return $this;
 			}
@@ -34,7 +34,7 @@ class WorkspaceConfig {
 				rename($tmp_config_git_dir.'/.git', CONFIG_DIR.'/.git');
 				File::rrmdir($tmp_config_git_dir);
 				chdir(CONFIG_DIR);
-				#exec('git reset --hard');
+				exec('git reset --hard');
 			}
 			Logger::log("Cloned config repo $repo_name from $host", 1);
 			return $this;
@@ -43,13 +43,62 @@ class WorkspaceConfig {
 		return $this;
 	}
 
+	public function setWorkspaceRepo()
+	{
+		if (is_dir('/workspace/.git') || ! is_file('/workspace/.system/upstream-workspace-repo'))
+		{
+			return $this;
+		}
+
+		exec("git clone \"$repo\" \"/workspace/.workspace-git\"");
+		rename("/workspace/.workspace-git/.git", "/workspace/.git");
+		File::rrmdir("/workspace/.workspace-git");
+		chdir('/workspace');
+		exec('git reset --hard');
+	}
+
 	public function installOhMyZsh($config_file)
 	{
 		if ( is_dir(CONFIG_DIR.'/oh-my-zsh'))
 		{
 			return $this;
 		}
-		$config = json_decode(file_get_contents($config_file), TRUE);
+
+		$config = is_file($config_file) ? json_decode(file_get_contents($config_file), TRUE) : array();
+		$oh_my_zsh_git = array_key_exists('repo', $config) ? $config['repo'] : 'https://github.com/crobays/oh-my-zsh.git';
+
+		Logger::log("Cloning $oh_my_zsh_git ...");
+		exec("git clone $oh_my_zsh_git ".CONFIG_DIR."/oh-my-zsh");
+		
+		return $this;
+	}
+
+	public function setZshrc()
+	{
+		if (is_file($config_zshrc = CONFIG_DIR.'/zshrc'))
+		{
+			return $this;
+		}
+
+		$theme = 'crobays';
+		$plugins = array(
+			'git',
+			'docker',
+			'composer',
+			'rvm',
+		);
+		
+		$template = File::read(CONFIG_DIR.'/oh-my-zsh/templates/zshrc.zsh-template');	
+		
+		$template = preg_replace('/export\sZSH=.*/', 'export ZSH="$CONFIG_DIR/oh-my-zsh"', $template);
+		$template = preg_replace('/plugins=\(.*\)/', 'plugins=('.implode(' ', $plugins).')', $template);
+		is_file(CONFIG_DIR."/oh-my-zsh/themes/$theme.zsh-theme") && ($template = preg_replace('/ZSH_THEME=".*"/', "ZSH_THEME=\"$theme\"", $template));
+		is_file(CONFIG_DIR.'/shell-profile-workspace') && ($template .= 'source "$CONFIG_DIR/shell-profile-workspace"'."\n");
+
+		File::write($config_zshrc, $template);
+		
+		is_file($user_zshrc = $_SERVER['HOME'].'/.zshrc') && unlink($user_zshrc);
+		symlink($config_zshrc, $user_zshrc);
 		return $this;
 	}
 }
--- workspace-image/run.sh
+++ workspace-image/run.sh
@@ -55,16 +55,13 @@ fi
 
 if [ ! -d "$CONFIG_DIR" ]
 then
-	cp --recursive /root/config-boilerplate "$CONFIG_DIR"
+	cp -R /root/config-boilerplate "$CONFIG_DIR"
 fi
 
 for file in $(ls /etc/my_init.d)
 do
 	/etc/my_init.d/"$file"
 done
-
-OH_MY_ZSH_GIT="https://github.com/crobays/oh-my-zsh.git"
-OH_MY_ZSH_THEME="crobays"
 
 if [ "$(echo $LOG | awk '{print tolower($0)}')" == "false" ] || [ "$LOG" == "0" ]
 then
@@ -79,6 +76,7 @@ then
 fi
 
 USERNAME="${USERNAME:-me}"
+su "$USERNAME" --command "$SCRIPTS/config-scripts/bootstrap.php"
 
 if [ ! $(getent passwd "$USERNAME") ]
 then
@@ -122,53 +120,10 @@ then
 	# Remove the need for entering a password at sudo
 	sed -i "s/%sudo	ALL=(ALL:ALL) ALL/%sudo	ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers
 
-	# Install oh-my-zsh
-	if [ ! -d "$CONFIG_DIR/oh-my-zsh" ]
-	then
-		if [ $LOG ]
-		then
-		    info "Cloning $OH_MY_ZSH_GIT ..."
-		fi
-		su "$USERNAME" --command "git clone $OH_MY_ZSH_GIT $CONFIG_DIR/oh-my-zsh"
-	fi
-
-	# Config oh-my-zsh
-	if [ ! -f "$CONFIG_DIR/zshrc" ]
-	then
-		cp -f "$CONFIG_DIR/oh-my-zsh/templates/zshrc.zsh-template" "$CONFIG_DIR/zshrc"
-		sed -i "s/export ZSH=\$HOME\/\.oh-my-zsh/export ZSH=${CONFIG_DIR//\//\\\/}\/oh-my-zsh/" "$CONFIG_DIR/zshrc"
-		sed -i "s/plugins=(git)/plugins=(git docker composer rvm)/" "$CONFIG_DIR/zshrc"
-		if [ "$OH_MY_ZSH_THEME" != "" ]
-		then
-			info "Set theme to $OH_MY_ZSH_THEME ..."
-			if [ -f "$CONFIG_DIR/oh-my-zsh/themes/$OH_MY_ZSH_THEME.zsh-theme" ]
-			then
-				sed -i "s/ZSH_THEME=\".*\"/ZSH_THEME=\"$OH_MY_ZSH_THEME\"/" "$CONFIG_DIR/zshrc"
-			else
-				echo "Unkown theme: $OH_MY_ZSH_THEME in $CONFIG_DIR/oh-my-zsh/themes"
-			fi
-		fi
-		if [ -f "$CONFIG_DIR/shell-profile-workspace" ]
-		then
-			echo 'source "$CONFIG_DIR/shell-profile-workspace"' >> "$CONFIG_DIR/zshrc"
-		fi
-	fi
-
-	ln --symbolic --force "$CONFIG_DIR/zshrc" "$userhome/.zshrc"
-
 	# Config
 	if [ -f "$SCRIPTS/config-scripts/bootstrap.php" ]
 	then
 		su "$USERNAME" --command "$SCRIPTS/config-scripts/bootstrap.php"
-	fi
-
-	if [ ! -d "/workspace/.git" ] && [ -f "/workspace/.system/upstream-workspace-repo" ]
-	then
-		su "$USERNAME" --command "git clone \"$repo\" \"/workspace/.workspace-git\""
-		mv "/workspace/.workspace-git/.git" "/workspace/.git"
-		rm -rf "/workspace/.workspace-git"
-		# rm -f "/workspace/.system/upstream-workspace-repo"
-		# su "$USERNAME" --command "cd /workspace && git reset --hard"
 	fi
 
 	info "Hi $USERNAME, your password is $USERNAME. (root=root)"
